d098364920bb10e54a57764af4ae920ed0333fc4
Maurice Miema <159928035+Maurice-Miema@users.noreply.github.com>
2025-03-19 23:57:04 +0100

    Update index.php

--- server/public/index.php
+++ server/public/index.php
@@ -3,8 +3,8 @@
     ini_set('display_errors', 1);
     
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST");
-    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
     require_once __DIR__ . "/../routes/web.php";
 ?>
